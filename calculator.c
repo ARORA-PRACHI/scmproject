7a4531bf8abb01bb7c270b7d1665767be5c0a6c6
prachiarora <prachiarora0926@gmail.com>
2023-03-21 19:55:33 +0530

    added

--- calculator.c
+++ calculator.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <math.h>
+
 int main() {
    char operator;
    double num1, num2, result;
 
-   printf("Enter an operator (+, -, *, / ");
+   printf("Enter an operator (+, -, *, /, %%, ^, s): ");
    scanf("%c", &operator);
 
    switch(operator) {
@@ -43,7 +44,47 @@ int main() {
                }
                break;
          }
-        
+         break;
+
+      case '%':
+         printf("Enter the first number: ");
+         scanf("%lf", &num1);
+
+         printf("Enter the second number: ");
+         scanf("%lf", &num2);
+
+         if (fmod(num1, num2) == 0) {
+            printf("%.2lf is divisible by %.2lf", num1, num2);
+         } else {
+            printf("%.2lf is not divisible by %.2lf", num1, num2);
+         }
+         break;
+
+      case '^':
+         printf("Enter the base: ");
+         scanf("%lf", &num1);
+
+         printf("Enter the exponent: ");
+         scanf("%lf", &num2);
+
+         result = pow(num1, num2);
+         printf("%.2lf ^ %.2lf = %.2lf", num1, num2, result);
+         break;
+
+      case 's':
+         printf("Enter the number: ");
+         scanf("%lf", &num1);
+
+         if (num1 < 0) {
+            printf("Error! Square root of a negative number is not allowed.");
+         } else {
+            result = sqrt(num1);
+            printf("Square root of %.2lf = %.2lf", num1, result);
+         }
+         break;
+
+      default:
+         printf("Error! Invalid operator.");
    }
 
    return 0;
